3189815b5e31b7face6c642f71047e00b0d632c7
RinFroster <masterofblade69@gmail.com>
2021-04-11 22:40:23 +0700

    upload popup video code

--- src/Redux/Reducer/index.js
+++ src/Redux/Reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import listMovieReducerNS from "./../../Component/MovieItem/modules/reducer";
 import listMovieReducerCS from "./../../Component/MovieItemCS/modules/reducer";
+import listMovieCarouselReducer from "./../../Component/Carousel/modules/reducer";
 
 const rootReducer = combineReducers({
     listMovieReducerNS,
     listMovieReducerCS,
+    listMovieCarouselReducer,
 });
 
 export default rootReducer;
