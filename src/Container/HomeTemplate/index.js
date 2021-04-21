aba8f967c59d2038d95f5c9ed48ee98f17147b09
leducbinh <lebinh199>
2021-04-21 19:04:36 +0700

    fix code add

--- src/Container/HomeTemplate/index.js
+++ src/Container/HomeTemplate/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import NavbarHome from "./../../Component/NavbarHome";
 import Footer from "../../Component/Footer";
-<<<<<<< HEAD
-// import { scrollToTop } from "react-scroll/modules/mixins/animate-scroll";
-=======
 import BackToTopButton from "./../../Component/BackToTopButton";
->>>>>>> 5b40912329ed30ccdc78ce9a310b9b989ed63d1c
-
 
 function HomeLayout(props) {
   return (
